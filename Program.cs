d8f4c536f803c95f24654ee5f1cfcd0b1fae8179
lisorom <melgarejolautar@gmail.com>
2026-04-24 10:36:23 -0300

    Add files via upload
    ## 6. Desafío de Aplicación
    
    **El Sistema de Cajero:**
    
    Crea un programa para un cajero automático que contenga:
    
    1. Un método `void` para mostrar el menú.
    2. Una función que reciba el saldo actual y el monto a retirar, y **retorne** el nuevo saldo (validando que haya fondos).
    3. Un método que use un parámetro `out` para devolver al mismo tiempo: si la operación fue exitosa (`bool`) y cuántos billetes se entregaron (`int`).

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double saldo;
+        double saca;
+
+        Console.WriteLine("Cuanto saldo posee: ");
+        saldo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese cuánto va a retirar: ");
+        saca = double.Parse(Console.ReadLine());
+
+        Retirar(ref saldo, saca, out double plata);
+
+        if (saldo < 0)
+        {
+            Console.WriteLine("Error al retirar un monto");
+        }
+        else
+        {
+            Console.WriteLine($"Su saldo actual es de {saldo}");
+        }
+    }
+    static bool Retirar(ref double saldoActual, double montoRetirar, out double CantidadPlata)
+    {
+        if (montoRetirar <= saldoActual)
+        {
+            saldoActual -= montoRetirar;
+            CantidadPlata = montoRetirar;
+            Console.WriteLine($"La operacion fue exitosa, recibiste {montoRetirar} en billetes");
+            return true;
+        }
+        else
+        {
+            CantidadPlata = 0;
+            Console.WriteLine("sin platita");
+            return false;
+        }
+    }
+}
