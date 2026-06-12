de26e7ac8176114fe70083ffa1872276fc034332
lisorom <melgarejolautar@gmail.com>
2026-06-12 11:40:39 -0300

    Add files via upload

---
+++ Reto desafio de matrices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+int[,] matriz = new int[3 , 3];
+int numeros = 2;
+
+
+for (int i = 0; i < matriz.GetLength(0); i++) 
+{
+    for (int j = 0; j < matriz.GetLength(0); i++) 
+    {
+
+        matriz [i,j] = numeros;
+        numeros++;
+
+        if (j % 2 != 0)
+        {
+            matriz[i, j] = numeros++;
+            numeros++;
+        }
+    }
+}
+
+
